ffa8c2a97f433b2dee5b7908f26008362bfd1d3b
Bognar Junior <bognar_junior@yahoo.com.br>
2018-01-15 23:44:29 -0200

    Aulas 32, 33, 34, 35, 36
    Ajustes no formulário de contato

--- HTML5/_javascript/funcoes.js
+++ HTML5/_javascript/funcoes.js
@@ -2,3 +2,9 @@ function mudaFoto(foto)
 {
     document.getElementById("icone").src = foto;
 }
+
+function calc_total() {
+    var qtd = parseInt(document.getElementById('cQtd').value);
+    var total = qtd * 1500;
+    document.getElementById('cTot').value = total;
+}
